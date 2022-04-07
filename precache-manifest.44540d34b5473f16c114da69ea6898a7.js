7d9e4bdab2294a00e59a2c7202a985f062d54191
VIKRANT KUMAR <signintovikrant150@gmail.com>
2022-04-07 14:58:06 +0530

    Updates

---
+++ precache-manifest.44540d34b5473f16c114da69ea6898a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e91d5497ef4c78fccc86",
+    "url": "/TEXTUTILS-REACT/static/css/main.a757ebee.chunk.css"
+  },
+  {
+    "revision": "e91d5497ef4c78fccc86",
+    "url": "/TEXTUTILS-REACT/static/js/main.e91d5497.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/TEXTUTILS-REACT/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "1639b18c62923541ce21",
+    "url": "/TEXTUTILS-REACT/static/js/2.1639b18c.chunk.js"
+  },
+  {
+    "revision": "bdc081aa67d9746832e2",
+    "url": "/TEXTUTILS-REACT/static/js/runtime~main.bdc081aa.js"
+  },
+  {
+    "revision": "95bd61d6841ace771303c6917244af11",
+    "url": "/TEXTUTILS-REACT/index.html"
+  }
+];
